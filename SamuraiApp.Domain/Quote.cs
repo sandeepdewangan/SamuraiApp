baca67db337d2e169ca2283673261028f6579a5d
sandeepdewangan <sandeepdewangan7@gmail.com>
2021-04-04 09:10:05 +0530

    Add project files.

---
+++ SamuraiApp.Domain/Quote.cs
@@ -0,0 +1,12 @@
+﻿namespace SamuraiApp.Domain
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        // Foreign Key to Samurai
+        public Samurai Samurai { get; set; }
+        public int SamuraiId { get; set; }
+    }
+}
